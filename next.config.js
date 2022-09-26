3b3b7281b7b27b2335eb4e2ed25bd97ebbfa9e88
sahilsinghsimform <sahilsinghsimform@gmail.com>
2022-09-26 17:15:20 +0530

    Adding Files

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["assets.pokemon.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
