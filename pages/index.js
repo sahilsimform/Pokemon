3f35da06c91afbb6cd67f56b6bcc8c1a5f2db8f2
sahilsinghsimform <sahilsinghsimform@gmail.com>
2022-10-19 14:50:32 +0530

    Edditing Index page

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,11 @@ import Layout from "../components/Layout";
 const Index = () => {
   return (
     <Layout title="Home">
-      <div>
-        <h1 className="text-red">Welcome To Sahil Pokemon App</h1>
+      <div className="border-1 rounded border-2 border-black  bg-white  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <h3 className="text-3xl">Welcome To Sahil Pokemon App</h3>
+      </div>
+      <div className="border-1 rounded border-2 border-black  bg-white  dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <h3 className="text-xl">To see the PokemonList Please Sigin/Signup </h3>
       </div>
     </Layout>
   );
